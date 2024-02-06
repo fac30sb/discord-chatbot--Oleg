c497bd3f74d687b809bc89bbb515c9d545aacc9e
zekvantum <zekvantum@zekvantum.com>
2024-02-06 18:39:22 +0000

    fixed issue with not responding to direct messages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, IntentsBitField, Partials } = require('discord.js');
 
-const client = new Client({
-  intents: [
+
+const client = new Client
+({
+  intents: 
+  [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.GuildMessageTyping,
     IntentsBitField.Flags.DirectMessages,
     IntentsBitField.Flags.DirectMessageReactions,
     IntentsBitField.Flags.DirectMessageTyping,
   ],
+  partials: 
+  [
+    Partials.Message, 
+    Partials.Channel, 
+    Partials.Reaction
+  ]
 });
+
 
 client.on('ready', (c) => {
   console.log(`✅ ${c.user.tag} is online.`);
@@ -19,13 +30,18 @@ client.on('ready', (c) => {
 
 
 client.on('messageCreate', message => {
+  console.log(`Received message from ${message.author.tag} in ${message.guild ? `guild ${message.guild.name}` : 'DM'}. Content: ${message.content}`);
+  
   if (message.author.bot) return;
+  
   if (message.content === 'ping') {
     message.channel.send('Pong!');
   }
+  
   if (message.content === 'hello') {
     message.channel.send('Hello!');
   }
 });
+
 
 client.login(process.env.DISCORD_TOKEN);
